c1a42524345d9fa5594ca973008daff4fe835af8
doyamd <doiamdissa94@gmail.com>
2025-05-21 15:22:35 +0300

    Add route for CaseRequestListCreateAV to handle case requests

--- backend/legalCase/API/urls.py
+++ backend/legalCase/API/urls.py
@@ -4,4 +4,5 @@ from legalCase.API.views import *
 urlpatterns = [
     path('cases', CaseListCreateAV.as_view(), name='case-list-create'),
     path('cases/<uuid:pk>', CaseDetailAV.as_view(), name='case-detail'),
+    path('case-requests', CaseRequestListCreateAV.as_view(), name='case-request-list-create'),
 ]
