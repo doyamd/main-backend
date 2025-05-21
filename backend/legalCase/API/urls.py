fb6b6457e4b23c6beacbc265a218be4177aa2451
doyamd <doiamdissa94@gmail.com>
2025-05-21 16:10:10 +0300

    Add URL route for CaseRequestDecisionView to handle case request decisions

--- backend/legalCase/API/urls.py
+++ backend/legalCase/API/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('cases', CaseListCreateAV.as_view(), name='case-list-create'),
     path('cases/<uuid:pk>', CaseDetailAV.as_view(), name='case-detail'),
     path('case-requests', CaseRequestListCreateAV.as_view(), name='case-request-list-create'),
+    path('case-requests/<uuid:pk>', CaseRequestDecisionView.as_view(), name='case-request-decision'),
 ]
