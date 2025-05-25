e2848613330e585c23e19b2214c08db3291afe51
doyamd <doiamdissa94@gmail.com>
2025-05-25 19:11:11 +0300

    Add signals import in LegalanalyticsConfig ready method

--- backend/legalAnalytics/apps.py
+++ backend/legalAnalytics/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class LegalanalyticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'legalAnalytics'
+
+    def ready(self):
+        import legalAnalytics.signals
+ 
