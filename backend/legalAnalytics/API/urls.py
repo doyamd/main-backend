cd49740d89bc9da420150c7d1fee932ecf4c16ff
doyamd <doiamdissa94@gmail.com>
2025-05-25 19:10:54 +0300

    Add URL routes for daily, monthly, and lifetime analytics views

--- backend/legalAnalytics/API/urls.py
+++ backend/legalAnalytics/API/urls.py
@@ -1,7 +1,10 @@
 # analytics/urls.py
 from django.urls import path
-from .views import AnalyticsView
+from .views import *
 
 urlpatterns = [
     path("realtime", AnalyticsView.as_view(), name="realtime-analytics"),
+    path("daily", DailyAnalyticsView.as_view(), name="daily-analytics"),
+    path("monthly", MonthlyAnalyticsView.as_view(), name="monthly-analytics"),
+    path("lifetime", LifetimeAnalyticsView.as_view(), name="lifetime-analytics")
 ]
