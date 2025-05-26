ebdd812009400839a18d5dbdbd94d3ae84a7815d
doyamd <doiamdissa94@gmail.com>
2025-05-27 00:55:46 +0300

    Add route for AttorneyWithCasesAPIView to retrieve attorneys with cases

--- backend/legalCase/API/urls.py
+++ backend/legalCase/API/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cases/<uuid:pk>', CaseDetailAV.as_view(), name='case-detail'),
     path('case-requests', CaseRequestListCreateAV.as_view(), name='case-request-list-create'),
     path('case-requests/<uuid:pk>', CaseRequestDecisionView.as_view(), name='case-request-decision'),
+    path('report/attorneys-with-cases', AttorneyWithCasesAPIView.as_view(), name='attorneys-with-cases'),
 ]
